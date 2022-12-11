1b0c8a2607b093a4c3ebf2d66117703a95147346
Diego Molejon <102906131+gitmolejon@users.noreply.github.com>
2022-12-11 18:28:02 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 console.log("🔥 Inserted script fired");
 document.querySelector('img[alt="logo"]').style.backgroundColor = "red";
+window.IS_MOBILE_APP ? console.log("📱 Mobile phone!") : console.log("Not mobile device");
 console.log("✅ Inserted script End");
